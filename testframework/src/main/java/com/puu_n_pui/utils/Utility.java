a34c180e3adbbb3fb5d9410cd0849fd8cdda5f80
Amit Basak <amitbasak@infrrd.ai>
2018-04-20 15:16:41 +0530

    changes in webdriver

--- testframework/src/main/java/com/puu_n_pui/utils/Utility.java
+++ testframework/src/main/java/com/puu_n_pui/utils/Utility.java
@@ -9,6 +9,7 @@ public class Utility {
 	public static WebDriver selectBrowser(String browser) {
 		WebDriver driver = null;
 		WebDriver driver1=null;
+		WebDriver driver2=null;
 		if (browser.equalsIgnoreCase("Chrome")) {
 			System.setProperty("webdriver.chrome.driver", "../../DRIVERS/chromedriver.exe");
 			driver = new ChromeDriver();
@@ -17,6 +18,7 @@ public class Utility {
 			driver = new FirefoxDriver();
 		}
 		System.out.println("hello");
+		
 		return driver;
 	}
 
